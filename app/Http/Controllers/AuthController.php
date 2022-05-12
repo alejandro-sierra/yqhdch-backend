3283ee540aff544421750334f9a44eaa5e54ce07
alejandro-sierra <alejandrosierraballesta@gmail.com>
2022-05-12 17:31:47 +0200

    adding function login to API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,32 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    public function login(Request $request){
+        $fields = $request -> validate([
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+        
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'https://http.cat/401'
+            ], 401);
+        }
+
+        $token = $user -> createToken('mytoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
