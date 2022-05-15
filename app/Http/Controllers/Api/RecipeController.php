acd3f9c087f8c1a3a10469ac33713c3bf11caafb
alejandro-sierra <alejandrosierraballesta@gmail.com>
2022-05-15 23:22:24 +0200

    [YQHDCH-8] feat: endpoint create and delete done

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
+use RoachPHP\Shell\Repl;
 
 class RecipeController extends Controller
 {
@@ -13,7 +14,7 @@ class RecipeController extends Controller
         return Recipe::all();
     }
     
-    public function recipeRandom()
+    public function recipe()
     {
         // TODO: Revisar como hacer el random
         /*
@@ -22,6 +23,11 @@ class RecipeController extends Controller
         $recipeRandom = rand(min($recipe), max($recipe));
         
         $recipe = Recipe::find($recipeRandom);
+
+        public function random()
+        {
+            return Chollo::inRandomOrder()->limit(1)->get();
+        }
         */
 
         $recipe = Recipe::find(1);
@@ -51,8 +57,54 @@ class RecipeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    
+    public function create(Request $request)
     {
-        //
+        $request -> validate([
+            'title' => 'required|string',
+            'preparation' => 'required|string',
+            'difficulty' => 'required|string',
+            'preparation_time' => 'required|integer',
+            'diet' => 'required|string',
+        ]);
+
+        $recipe = new Recipe();
+
+        $recipe->title = $request->title;
+        $recipe->preparation = $request->preparation;
+        $recipe->difficulty = $request->difficulty;
+        $recipe->preparation_time = $request->preparation_time;
+        $recipe->diet = $request->diet;
+        
+        $recipe->save();
+
+        $response = [
+            'recipe' => $recipe,
+        ];
+
+        return response($recipe, 201);
+    }
+
+
+    public function delete($id)
+    {
+        $recipe = Recipe::find($id);
+        if(!$recipe){
+            return response()->json(['error' => 'Receta no encontrado'], 404);
+        }else{
+            $recipe -> delete();
+            return response()->json(['content' => 'Chollo eliminado correctamente'], 200);
+        }
     }
 }
+
+
+// [
+//     {
+//         "tile": "Ponche segoviano, receta de uno de los pasteles tradicionales más suaves y delicados ",
+//         "preparation": "Comenzaremos el día anterior preparando el mazapán. Para ello mezclamos la harina de almendra, el azúcar y el azúcar invertido, removemos. Añadimos el agua y mezclamos hasta que adquiera una textura manejable. Envolvemos en papel film y dejamos reposar 12 horas en la nevera. Para el bizcocho, precalentamos el horno a 180º y forramos una bandeja con papel de horno. En un bol, batimos las yemas y la mitad del azúcar con unas varillas eléctricas durante unos 10 minutos o hasta que la mezcla blanquee y triplique su volumen. Añadimos las harinas tamizadas y la por último la leche, removemos hasta que no queden grumos. Por otra parte, y también con unas varillas batimos las claras hasta formar un merengue con el resto del azúcar, mezclamos a la mezcla anterior suavemente con movimientos envolventes. Pasamos la mezcla a la bandeja de horno y extendemos para igualar por todos lados. Horneamos unos 12 minutos y cuando esté listo dejamos enfriar. Después cortamos los bordes para igualar el bizcocho y lo cortamos en tres partes iguales. Reservamos hasta el montaje. Para el almíbar, mezclamos el agua y el azúcar hasta que llegue a hervir. Reservamos hasta que enfríe. Para la yema pastelera. En un bol mezclamos el azúcar con la Maicena. Añadimos los huevos bien batidos colándolos por un colador, y hacemos con esto una papilla. Ponemos un cazo al fuego con el agua, cuando esta llegue a 40 grados centígrados, añadimos la papilla anterior al agua caliente y sin dejar de remover con unas varillas cocinamos hasta que espese, unos 7 minutos más o menos. Retiramos del fuego, y tapamos a piel con un fim de cocina . Reservamos hasta que enfríe. ",
+//         "difficulty": "media",
+//         "preparation_time": 120,
+//         "diet": "vegetariano"
+//     }
+// ]
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 const BASE_ROUTE_RECIPE = "/recipes";
-const GETALL_RECIPES = BASE_ROUTE_RECIPE . "";
+const GETALL = BASE_ROUTE_RECIPE . "";
+const CREATE = BASE_ROUTE_RECIPE . "/create";
+const DELETE = BASE_ROUTE_RECIPE . "/delete/{id?}";
 
 
 // Public routes
@@ -14,10 +16,12 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 
-Route::get(GETALL_RECIPES, [RecipeController::class, 'recipes'])->name('recipes');
+Route::get(GETALL, [RecipeController::class, 'recipes'])->name('recipes');
+Route::post(CREATE, [RecipeController::class, 'create'])->name('create');
+Route::delete(DELETE, [RecipeController::class, 'delete'])->name('delete');
 
 
-Route::get('/recipeRandom', [RecipeController::class, 'recipeRandom'])->name('recipeRandom');
+Route::get('/recipe', [RecipeController::class, 'recipe'])->name('recipe');
 
 
 // Protected routes
