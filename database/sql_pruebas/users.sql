02f61eff2cc000cebaa1f5b79b743e7d0e912a38
alejandro-sierra <alejandrosierraballesta@gmail.com>
2022-05-28 11:19:18 +0200

    [YQHDCH-9] chore: change password mariadb

--- database/sql_pruebas/users.sql
+++ database/sql_pruebas/users.sql
@@ -22,3 +22,9 @@ insert into users value(null, "Andrea", "user", "andrea@user.com", null, "$2y$10
 
 insert into users value(null, "Jose Juan", "admin", "josejuan@admin.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
 insert into users value(null, "Jose Juan", "user", "josejuan@user.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
+
+insert into users value(null, "Pakita", "admin", "pakita@admin.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
+insert into users value(null, "Pakita", "user", "pakita@user.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
+
+insert into users value(null, "Lenche", "admin", "lenche@admin.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
+insert into users value(null, "Lenche", "user", "lenche@user.com", null, "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", null, current_time(), current_time() );
